e4098079b2fc1039c6e40b2b2a6c5c78e08117bf
Alex Antonio <alexantonio308@gmail.com>
2020-03-07 18:06:58 -0300

    Código Rodando

--- 14mai.py
+++ 14mai.py
@@ -9,7 +9,7 @@ from sklearn import metrics
 import matplotlib.pyplot as plt
 
 #Leitura do Arquivo de dados
-base = pd.read_csv("C:/Users/alexa/Desktop/I.A/items.csv", sep=',')
+base = pd.read_csv("C:/Users/alexa/Desktop/Regressao/items.csv", sep=',')
 print("Número de linhas/Colunas iniciais: \n", base.shape)
 
 #Excluir linhas com dados Nan
@@ -20,34 +20,36 @@ print("Número de linhas/Colunas removendo Nan: \n", base.shape)
 print("\nDescrição da tabela :\n",base.describe())
 
 #Adicionar dados em arrays
-rm = np.array(base["rating"]).reshape(-1,1)
-medv = np.array(base["totalReviews"]).reshape(-1,1)
-
+#X = pd.DataFrame(base,columns=["rating","totalReviews"])
+#y = pd.DataFrame(base["prices"])
+X = np.array(base["rating"]).reshape(-1,1)
+y = np.array(base["totalReviews"]).reshape(-1,1)
 
 #Test_size = 20% de taxa de treino
-X_train, X_test, y_train, y_test = train_test_split(rm, medv, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 #Treinamento
 linreg= LinearRegression()
 linreg.fit(X_train, y_train)
-linreg.score(X_test,y_test)
 
 #Valor de inclinação
 print('\nCoeficiente de Inclinação (w): {}'.format(linreg.coef_))
  #Valores de interceptação
 print(list(map('Linha de Interceptação (b): {:.3f}'.format,linreg.intercept_)))
- 
-print('R-squared score (treinamento): {:.3f}'.format(linreg.score(X_train, y_train)))
- 
-print('R-squared score (test): {:.3f}\n\n'.format(linreg.score(X_test, y_test)))
+
 
 #Previsões
 y_pred = linreg.predict(X_test)
-df = pd.DataFrame({'Atual': y_test.flatten(), 'Previsão': y_pred.flatten()})
-print(df)
+#df = pd.DataFrame({'Atual': y_test.flatten(), 'Previsão': y_pred.flatten()})
+#print(df)
 
+#Validação cruzada
+medias = cross_val_score(linreg, X_test, y_test, cv=5)
+media = sum(medias) / len(medias)
+print("VAlidação Cruzada :\n",media)
 #Erro médio
 print('\nErro médio Quadrático:', metrics.mean_squared_error(y_test, y_pred)) 
+
 #Plotagem dos Gráficos
 #Gráfico com dados de treinamento
 plt.scatter(X_train, y_train, marker='D', s=40, alpha=0.9)
